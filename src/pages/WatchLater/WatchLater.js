2193ebc9bab2200c08d6c9280114aa232421bd2f
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-05-10 16:49:56 -0300

    Create Page Watch Later for saved films

--- src/pages/WatchLater/WatchLater.js
+++ src/pages/WatchLater/WatchLater.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
+import { MdDelete } from "react-icons/md";
 
 export default function WatchLater(){
     const [films, setFilms] = useState([]);
 
     useEffect(()=>{
         const getFilms = JSON.parse(localStorage.getItem("@postersfilm"));
-        setFilms([...films, ...getFilms]);
+        setFilms(getFilms);
     }, []);
     
     return(
@@ -14,7 +15,11 @@ export default function WatchLater(){
                 films.map((film)=>{
                     return(
                         <div key={film.id}>
+                            <img src={`https://image.tmdb.org/t/p/original/${film.backdrop_path}`} alt="Poster Film"/>
                             <h1>{film.title}</h1>
+                            <button>
+                                <MdDelete />
+                            </button>
                         </div>
                     )
                 })
