716cde33fe6583253bebeae12622530adafe948c
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-05-13 15:35:13 -0300

    Create a conditional rendering

---
+++ src/components/EmptyList/EmpytList.js
@@ -0,0 +1,7 @@
+export default function EmptyList(){
+    return(
+        <>
+            <h1>Save films to watch later...</h1>
+        </>
+    );
+}
--- src/pages/WatchLater/WatchLater.js
+++ src/pages/WatchLater/WatchLater.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { MdDelete } from "react-icons/md";
+import EmptyList from "../../components/EmptyList/EmpytList";
 
 export default function WatchLater(){
     const [films, setFilms] = useState([]);
@@ -8,27 +9,21 @@ export default function WatchLater(){
         const getFilms = JSON.parse(localStorage.getItem("@postersfilms"));
         setFilms(getFilms);
     }, []);
-    
-    function deleteFilm(){
-        const deletedFilm = films.find((film)=> film.id !== films.id);
-        setFilms(deletedFilm);
-        localStorage.setItem("@postersfilms", JSON.stringify(deletedFilm));
-    }
 
     return(
         <>
             {
-                films.map((film)=>{
+                films? films.map((film)=>{
                     return(
                         <div key={film.id}>
                             <img src={`https://image.tmdb.org/t/p/original/${film.backdrop_path}`} alt="Poster Film"/>
                             <h1>{film.title}</h1>
-                            <button onClick={deleteFilm}>
+                            <button>
                                 <MdDelete />
                             </button>
                         </div>
                     )
-                })
+                }) : <EmptyList/>
             }
         </>
     );
