cb958d11b102aac9ba5c4a07222c008677e6a83c
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-05-14 09:36:09 -0300

    Ceate a delete function on page

--- src/pages/WatchLater/WatchLater.js
+++ src/pages/WatchLater/WatchLater.js
@@ -10,6 +10,12 @@ export default function WatchLater(){
         setFilms(getFilms);
     }, []);
 
+    function deleteFilms(id){
+        const newFilms = films.filter(film => film.id !== id);
+        setFilms(newFilms);
+        localStorage.setItem("@postersfilms", JSON.stringify(newFilms));
+    }
+
     return(
         <>
             {
@@ -18,7 +24,7 @@ export default function WatchLater(){
                         <div key={film.id}>
                             <img src={`https://image.tmdb.org/t/p/original/${film.backdrop_path}`} alt="Poster Film"/>
                             <h1>{film.title}</h1>
-                            <button>
+                            <button onClick={() => deleteFilms(film.id)}>
                                 <MdDelete />
                             </button>
                         </div>
