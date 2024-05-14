8d7e4907fb7fd4aba1bf4734c55a43281b251e22
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-05-14 09:35:15 -0300

    Create a Empty component for a watch later page

--- src/components/EmptyList/EmpytList.js
+++ src/components/EmptyList/EmpytList.js
@@ -1,6 +1,9 @@
+import { BiSolidCameraMovie } from "react-icons/bi";
+
 export default function EmptyList(){
     return(
         <>
+            <BiSolidCameraMovie />
             <h1>Save films to watch later...</h1>
         </>
     );
