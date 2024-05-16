a8b9cb8eb4f79e287dc25f34fe5d2589e69711a8
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-05-16 09:35:36 -0300

    Config React Toastify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import FilmsRoute from "./routes";
+import { ToastContainer } from "react-toastify";
+
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
   return (
+    <>
+      <ToastContainer autoClose={3000}/>
       <FilmsRoute/>
+    </>
   );
 }
